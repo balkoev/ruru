99d78da01f4feda05e3f7574045eb6632fdd006f
balkoev <adam22185515@gmail.com>
2019-09-29 15:11:51 +0300

    add form with semantic

---
+++ src/src/CreditCardForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, Checkbox, Form, Grid } from 'semantic-ui-react';
+
+const CardForm = () => (
+  <Grid centered columns={3}>
+    <Grid.Column>
+      <Form>
+        <Form.Field>
+          <label>PAN</label>
+          <input placeholder='Card number' />
+        </Form.Field>
+        <Form.Field>
+          <label>Cardholder</label>
+          <input placeholder='Full name' />
+        </Form.Field>
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='Expires' placeholder='mm/yy' />
+          <Form.Input fluid label='CVC' placeholder='Code' />
+        </Form.Group>
+        <Form.Field>
+          <Checkbox label='I agree to the Terms and Conditions' />
+        </Form.Field>
+        <Button type='submit'>Pay</Button>
+      </Form>
+    </Grid.Column>
+  </Grid>
+);
+
+export default CardForm;
