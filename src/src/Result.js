2f785e7ca1c3da7ec3e10c8e193466d5e5c6906b
balkoev <adam22185515@gmail.com>
2019-09-30 18:00:11 +0300

    add button repeat

--- src/src/Result.js
+++ src/src/Result.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Segment, Label, Container, Grid, Icon, Button, Row, Header } from 'semantic-ui-react';
 
-const Result = () => {
-  return (
-    <div>
-      Result
-    </div>
-  );
+export default class Result extends Component {
+
+  onClickRepeat = () => {
+    this.props.history.push('/');
+  };
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Grid centered columns={3}>
+            <Grid.Row>
+              <Grid.Column>
+                <h1 className="ui header center aligned">Payment</h1>
+                <Container textAlign='center'>
+              </Container>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Segment placeholder>
+                <Header icon>
+                  <Icon name='smile outline green' />
+                  Payment was successful
+                </Header>
+                <Segment.Inline>
+                  <Button onClick={this.onClickRepeat} color='blue' animated='vertical'>
+                    <Button.Content visible>Repeat</Button.Content>
+                    <Button.Content hidden>
+                      <Icon name='repeat' />
+                    </Button.Content>
+                  </Button>
+                </Segment.Inline>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Container>
+      </div>
+    );
+  };
 };
-
-export default Result;
